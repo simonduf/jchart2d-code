3889ed3f048b191b0f013bf072684170929b6d10
Achim Westermann <achim.westermann@gmx.de>
2011-03-27 15:24:06 +0200

    - Just formatting.

--- test/info/monitorenter/gui/chart/traces/TestTrace2DLtd.java
+++ test/info/monitorenter/gui/chart/traces/TestTrace2DLtd.java
@@ -42,76 +42,77 @@ import junit.framework.TestSuite;
  * @author <a href="mailto:Achim.Westermann@gmx.de">Achim Westermann</a>
  * 
  */
-public class TestTrace2DLtd
-    extends TestCase {
-  /**
-   * Test suite for this test class.
-   * <p>
-   * 
-   * @return the test suite.
-   */
-  public static Test suite() {
+public class TestTrace2DLtd extends TestCase {
+	/**
+	 * Test suite for this test class.
+	 * <p>
+	 * 
+	 * @return the test suite.
+	 */
+	public static Test suite() {
 
-    TestSuite suite = new TestSuite();
-    suite.setName(TestTrace2DLtd.class.getName());
+		TestSuite suite = new TestSuite();
+		suite.setName(TestTrace2DLtd.class.getName());
 
-    suite.addTest(new TestTrace2DLtd("testMemoryLeak"));
+		suite.addTest(new TestTrace2DLtd("testMemoryLeak"));
 
-    return suite;
-  }
+		return suite;
+	}
 
-  /**
-   * Creates a test case with the given name.
-   * <p>
-   * 
-   * @param testName
-   *            the name of the test case.
-   */
-  public TestTrace2DLtd(final String testName) {
-    super(testName);
-  }
+	/**
+	 * Creates a test case with the given name.
+	 * <p>
+	 * 
+	 * @param testName
+	 *            the name of the test case.
+	 */
+	public TestTrace2DLtd(final String testName) {
+		super(testName);
+	}
 
-  /**
-   * Adds 1000000 <code>{@link TracePoint2D}</code> instances to a
-   * <code>{@link Trace2DLtd}</code> and asserts that not more points than
-   * <code>{@link Trace2DLtd#getMaxSize()} </code> are remaining in memory.
-   * <p>
-   * 
-   */
-  public void testMemoryLeak() {
-	  Chart2D dummyChart = new Chart2D();
-    int traceSize = 10;
-    ITrace2D trace = new Trace2DLtd(traceSize);
-    dummyChart.addTrace(trace);
-    long max = 1000000;
-    long percentModulo = max / 20;
-    System.out.println("Adding " + max + " points to a Trace2DLtd and a WeakHashMap...");
-    ITracePoint2D point;
-    Map<ITracePoint2D, String> weakMap = new WeakHashMap<ITracePoint2D, String>();
-    for (long i = 0; i < max; i++) {
-      point = new TracePoint2D(i, i);
-      trace.addPoint(point);
-      weakMap.put(point, point.toString());
-      if (i % percentModulo == 0) {
-        System.out.println((i * 100 / max) + " %");
-      }
-    }
-    System.out.println("Dropping point reference (but holding trace)...");
-    point = null;
-    // trace=null;
-    long keys = weakMap.size();
-    System.out.println("Points remaining in the weakMap: " + keys);
-    System.out.println("System.runFinalization()... ");
-    System.runFinalization();
-    System.out.println("System.gc()... ");
-    System.gc();
-    keys = 0;
-    for (ITracePoint2D pt : weakMap.keySet()) {
-      keys++;
-      System.out.println("Point " + pt.toString() + " was not dropped.");
-    }
-    System.out.println("Points remaining in the weakMap: " + keys);
-    Assert.assertFalse("There are " + keys + " TracePoint2D instances not deleted from the WeakHashMap.",
-        keys > traceSize);
-  }
+	/**
+	 * Adds 1000000 <code>{@link TracePoint2D}</code> instances to a
+	 * <code>{@link Trace2DLtd}</code> and asserts that not more points than
+	 * <code>{@link Trace2DLtd#getMaxSize()} </code> are remaining in memory.
+	 * <p>
+	 * 
+	 */
+	public void testMemoryLeak() {
+		Chart2D dummyChart = new Chart2D();
+		int traceSize = 10;
+		ITrace2D trace = new Trace2DLtd(traceSize);
+		dummyChart.addTrace(trace);
+		long max = 1000000;
+		long percentModulo = max / 20;
+		System.out.println("Adding " + max
+				+ " points to a Trace2DLtd and a WeakHashMap...");
+		ITracePoint2D point;
+		Map<ITracePoint2D, String> weakMap = new WeakHashMap<ITracePoint2D, String>();
+		for (long i = 0; i < max; i++) {
+			point = new TracePoint2D(i, i);
+			trace.addPoint(point);
+			weakMap.put(point, point.toString());
+			if (i % percentModulo == 0) {
+				System.out.println((i * 100 / max) + " %");
+			}
+		}
+		System.out.println("Dropping point reference (but holding trace)...");
+		point = null;
+		// trace=null;
+		long keys = weakMap.size();
+		System.out.println("Points remaining in the weakMap: " + keys);
+		System.out.println("System.runFinalization()... ");
+		System.runFinalization();
+		System.out.println("System.gc()... ");
+		System.gc();
+		keys = 0;
+		for (ITracePoint2D pt : weakMap.keySet()) {
+			keys++;
+			System.out.println("Point " + pt.toString() + " was not dropped.");
+		}
+		System.out.println("Points remaining in the weakMap: " + keys);
+		Assert.assertFalse("There are " + keys
+				+ " TracePoint2D instances not deleted from the WeakHashMap.",
+				keys > traceSize);
+	}
 }
