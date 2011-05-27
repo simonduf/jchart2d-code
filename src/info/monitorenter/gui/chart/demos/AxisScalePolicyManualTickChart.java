b16b9502912c5c7d7b3313e31a360bdd87a04e87
Achim Westermann <achim.westermann@gmx.de>
2011-05-27 10:28:46 +0200

    ! New AxisScalePolicyManualTicks for controlling the ticks by hand.

---
+++ src/info/monitorenter/gui/chart/demos/AxisScalePolicyManualTickChart.java
@@ -0,0 +1,124 @@
+/*
+ *  MinimalStaticChart.java of project jchart2d, a demonstration 
+ *  of the minimal code to set up a chart with static data. 
+ *  Copyright (C) 2007 - 2011 Achim Westermann, created on 10.12.2004, 13:48:55
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  If you modify or optimize the code in a useful way please let me know.
+ *  Achim.Westermann@gmx.de
+ *
+ */
+package info.monitorenter.gui.chart.demos;
+
+import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.IAxis;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.axis.scalepolicy.AxisScalePolicyManualTicks;
+import info.monitorenter.gui.chart.traces.Trace2DSimple;
+import info.monitorenter.gui.chart.views.ChartPanel;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * A minimal example for a chart that has manual configured labels/ticks by using 
+ * <code>{@link AxisScalePolicyManualTicks}</code>.
+ * <p>
+ * 
+ * @author Achim Westermann
+ * 
+ * @version $Revision: 1.15 $
+ */
+public final class AxisScalePolicyManualTickChart extends JPanel {
+  /**
+   * Generated for <code>serialVersionUID</code>.
+   */
+  private static final long serialVersionUID = 3257009847668192307L;
+
+  /**
+   * Main entry.
+   * <p>
+   * 
+   * @param args
+   *          ignored.
+   */
+  public static void main(final String[] args) {
+    for (int i = 0; i < 1; i++) {
+      JFrame frame = new JFrame("SampleChart");
+      frame.getContentPane().add(new AxisScalePolicyManualTickChart());
+      frame.addWindowListener(new WindowAdapter() {
+        /**
+         * @see java.awt.event.WindowAdapter#windowClosing(java.awt.event.WindowEvent)
+         */
+        @Override
+        public void windowClosing(final WindowEvent e) {
+          System.exit(0);
+        }
+      });
+      frame.setSize(600, 400);
+      frame.setLocation(i % 3 * 200, i / 3 * 100);
+      frame.setVisible(true);
+    }
+  }
+
+  /**
+   * Defcon.
+   */
+  private AxisScalePolicyManualTickChart() {
+    this.setLayout(new BorderLayout());
+    Chart2D chart = new Chart2D();
+    /*
+     * This does the trick to configure the ticks manually:
+     */
+    IAxis xAxis = chart.getAxisX();
+    xAxis.setAxisScalePolicy(new AxisScalePolicyManualTicks()); 
+    xAxis.setMajorTickSpacing(10);
+    xAxis.setMinorTickSpacing(1);
+    xAxis.setStartMajorTick(true);
+    
+    IAxis yAxis = chart.getAxisY();
+    yAxis.setAxisScalePolicy(new AxisScalePolicyManualTicks()); 
+    yAxis.setMajorTickSpacing(50);
+    yAxis.setMinorTickSpacing(10);
+    yAxis.setStartMajorTick(true);
+    
+    // Create an ITrace:
+    // Note that dynamic charts need limited amount of values!!!
+    // ITrace2D trace = new Trace2DLtd(200);
+    ITrace2D trace = new Trace2DSimple();
+    trace.setColor(Color.RED);
+
+    // Add the trace to the chart:
+    chart.addTrace(trace);
+
+    // Add all points, as it is static:
+    for (int i = 0; i <= 20; i++) {
+      trace.addPoint(i, 100.0/(i+1.0));
+    }
+
+    chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
+
+    // Make it visible:
+    this.add(new ChartPanel(chart), BorderLayout.CENTER);
+
+  }
+
+}
