048c3e2c513e9c57c350c994318b8cef7c3ba508
Achim Westermann <achim.westermann@gmx.de>
2011-11-19 20:00:03 +0100

    - Further tests.

---
+++ test/info/monitorenter/gui/chart/tracepoints/TestTracePoint2D.java
@@ -0,0 +1,41 @@
+/*
+ *  TestTracePoint2D.java of project jchart2d, <enterpurposehere>. 
+ *  Copyright (C) 2002 - 2011, Achim Westermann, created on Nov 19, 2011
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  If you modify or optimize the code in a useful way please let me know.
+ *  Achim.Westermann@gmx.de
+ *
+ *
+ * File   : $Source: /cvsroot/jchart2d/jchart2d/codetemplates.xml,v $
+ * Date   : $Date: 2009/02/24 16:45:41 $
+ * Version: $Revision: 1.2 $
+ */
+
+package info.monitorenter.gui.chart.tracepoints;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTracePoint2D {
+
+  @Test
+  public void testEquals() {
+    TracePoint2D one = new TracePoint2D(2.0, 1.0);
+    TracePoint2D two = new TracePoint2D(2.0, 1.0);
+    assertEquals(one, two);
+  }
+}
