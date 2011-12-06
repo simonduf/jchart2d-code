bcf1232f9cc908531fa57ea2ee7609059dceb7b6
Achim Westermann <achim.westermann@gmx.de>
2011-12-06 21:15:44 +0100

    - formatting and overcoming http://egit.eclipse.org/r/#change,2534

--- src/info/monitorenter/gui/chart/demos/ChartLargeDataZoomable.java
+++ src/info/monitorenter/gui/chart/demos/ChartLargeDataZoomable.java
@@ -67,8 +67,8 @@ public final class ChartLargeDataZoomable {
     chart.setUseAntialiasing(true);
     chart.enablePointHighlighting(true);
     trace.setPointHighlighter(new PointPainterDisc(6));
-     chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
-    
+    chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
+
     // Add all points, as it is static:
     Random random = new Random();
 
