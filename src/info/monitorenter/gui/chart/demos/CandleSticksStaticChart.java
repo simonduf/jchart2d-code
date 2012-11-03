d1a58a835fb9d1977c70475f1d024ea51ecba28f
achimwestermann <achim.westermann@gmx.de>
2012-11-03 16:49:10 +0100

    ! improved demo data.

--- src/info/monitorenter/gui/chart/demos/CandleSticksStaticChart.java
+++ src/info/monitorenter/gui/chart/demos/CandleSticksStaticChart.java
@@ -24,16 +24,22 @@
 package info.monitorenter.gui.chart.demos;
 
 import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.IRangePolicy;
 import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.labelformatters.LabelFormatterDate;
+import info.monitorenter.gui.chart.rangepolicies.RangePolicyFixedViewport;
 import info.monitorenter.gui.chart.tracepoints.CandleStick;
 import info.monitorenter.gui.chart.traces.Trace2DCandleSticks;
 import info.monitorenter.gui.chart.traces.Trace2DSimple;
 import info.monitorenter.gui.chart.views.ChartPanel;
+import info.monitorenter.util.Range;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -64,7 +70,8 @@ public final class CandleSticksStaticChart extends JPanel {
    */
   public static void main(final String[] args) {
     JFrame frame = new JFrame(CandleSticksStaticChart.class.getSimpleName());
-    frame.getContentPane().add(new CandleSticksStaticChart());
+    CandleSticksStaticChart stickChart = new CandleSticksStaticChart();
+    frame.getContentPane().add(stickChart);
     frame.addWindowListener(new WindowAdapter() {
       /**
        * @see java.awt.event.WindowAdapter#windowClosing(java.awt.event.WindowEvent)
@@ -74,33 +81,95 @@ public final class CandleSticksStaticChart extends JPanel {
         System.exit(0);
       }
     });
-    frame.setSize(300, 300);
+    frame.setSize(600, 300);
     frame.setVisible(true);
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e1) {
+      // TODO Auto-generated catch block
+      e1.printStackTrace();
+    }
+    stickChart.fill();
   }
 
+  
   /**
    * Defcon.
    */
   private CandleSticksStaticChart() {
     this.setLayout(new BorderLayout());
-    Chart2D chart = new Chart2D();
+    this.m_chart = new Chart2D();
+    this.add(new ChartPanel(this.m_chart), BorderLayout.CENTER);
+  }
+
+  private Chart2D m_chart;
+  void fill() {
+    this.m_chart.getAxisX().setFormatter(new LabelFormatterDate(new SimpleDateFormat("yyyy-MM-dd")));
 
     // Create an ITrace:
     ITrace2D trace = new Trace2DCandleSticks(new Trace2DSimple(), 6);
     trace.setColor(Color.RED);
 
     // Add the trace to the chart:
-    chart.addTrace(trace);
+    this.m_chart.addTrace(trace);
 
     // Add all points, as it is static:
-    for (int i = 0; i < 10; i++) {
-      trace.addPoint(new CandleStick(i, i-2, i+2, i+4, i-4));
-    }
+    Calendar cal = Calendar.getInstance();
+    // DAX 19.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 19);
+    cal.set(Calendar.MONTH, 10);
+    cal.set(Calendar.YEAR, 2012);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7413.69, 7380.64, 7430.12, 7363.50));
+    // 18.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 18);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7401.41, 7437.23, 7447.81, 7389.01));
+    // 17.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 17);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7381.08, 7394.55, 7399.99, 7367.59));
+    // 16.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 16);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7309.08, 7376.27, 7387.78, 7293.90));
+    // 15.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 15);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7237.40, 7261.25, 7302.23, 7237.40));
+    // 12.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 12);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7260.57, 7232.49, 7291.24, 7232.42));
+    // 11.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 11);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7192.75, 7281.7, 7305.46, 7182.31));
+    // 10.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 10);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7219.46, 7205.23, 7245.17, 7201.09));
+    // 09.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 9);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7306.33, 7234.53, 7308.13, 7221.50));
+    // 08.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 8);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7340.39, 7291.21, 7341.28, 7286.24));
+    // 05.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 5);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7327.7, 7397.87, 7409.78, 7312.79));
+    // 04.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 4);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7368.21, 7305.21, 7374.86, 7283.48));
+    // 03.10.2012
+    cal.set(Calendar.DAY_OF_MONTH, 3);
+    trace.addPoint(new CandleStick(cal.getTimeInMillis(), 7283.26, 7322.08, 7339.33, 7272.06));
 
-    chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
-    // Make it visible:
-    this.add(new ChartPanel(chart), BorderLayout.CENTER);
-
+    this.m_chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
+    
+    /*
+     * Ensuring visibility: 
+     */
+    cal.set(Calendar.DAY_OF_MONTH, 2);
+    double startTimeX = cal.getTimeInMillis();
+    cal.set(Calendar.DAY_OF_MONTH, 20);
+    double endTimeX = cal.getTimeInMillis();
+    
+    IRangePolicy rangePolicy = new RangePolicyFixedViewport(new Range(startTimeX, endTimeX));
+    this.m_chart.getAxisX().setRangePolicy(rangePolicy);
+    
   }
 
 }
